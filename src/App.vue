9592e77d0b1f67e90af994b694cd9498f52874c7
Michael Winchel <you@example.com>
2020-12-05 18:55:13 -0600

    added cards component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     
     <!--imports header attribute -->
     <Header headerTitle="Popular TV Shows"/>
+    <Card cards="Popular TV Shows" v:bind:cards = "movies"/>
   </div>
 </template>
 
@@ -10,13 +11,43 @@
 
 // imports header file
 import Header from './components/Header.vue'
+//import card file
+import Card from './components/Card.vue'
+// import axios
+//import axios from 'axios'
+
 
 //adds header to componets to be exported to app.vue
 export default {
   name: 'App',
   components: {
-      Header
-  }
+      Header,
+      Card
+  },
+  data()
+  {
+    return{ 
+      movies:[
+        {id:1, img:"", title:"", overview:""},
+        {id:2, img:"", title:"", overview:""},
+        {id:3, img:"", title:"", overview:""},
+        {id:4, img:"", title:"", overview:""}
+      ]
+
+    }
+  },
+ // mounted()
+  //{
+    // imports api from movie database
+    // saves object as res
+   // axios.get('https://api.themoviedb.org/3/tv/popular?api_key=099fb9a4f570109a23e62c114190f40d&language=en-US&page=1')
+    //.then( (res) => {
+     // this.movies = res.data.results;
+      //this.movies.splice(4)
+
+    //})
+  //}
+
 }
 </script>
 
